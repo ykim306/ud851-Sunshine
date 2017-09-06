d6fc26c5a280446c4af07e560d62785a1565f5d5
ykim306 <ykim306@gmail.com>
2017-09-06 19:50:55 -0400

    TODO 1-9 completed

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -15,9 +15,13 @@
  */
 package com.example.android.sunshine.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -65,8 +69,23 @@ public final class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String locationQuery) {
-        // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
-        return null;
+        // COMPLETED (1) Fix this method to return the URL used to query Open Weather Map's API
+        Uri tempUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(FORMAT_PARAM,format)
+                .appendQueryParameter(UNITS_PARAM,units)
+                .appendQueryParameter(DAYS_PARAM, String.valueOf(numDays))
+                .appendQueryParameter(QUERY_PARAM,locationQuery)
+                .build();
+        URL url = null;
+        try {
+            url = new URL(tempUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "URL = " + url);
+
+        return url;
     }
 
     /**
