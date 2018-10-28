37821117b541451f0cde19a9b95b94aa93fcb898
unknown <ykim306@gmail.com>
2018-10-28 18:11:52 -0400

    Ex S10.02 Completed

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,20 +15,32 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+    //  COMPLETED (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    //  COMPLETED (2) Create a synchronized public static void method called initialize
+    synchronized public static void initialize(Context context) {
+        //  COMPLETED (3) Only execute this method body if sInitialized is false
+        if (!sInitialized) {
+            new fillContentProvider(context).execute();
+            //  COMPLETED (4) If the method body is executed, set sInitialized to true
+            sInitialized = true;
+        }
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
@@ -39,5 +51,43 @@ public class SunshineSyncUtils {
     public static void startImmediateSync(@NonNull final Context context) {
         Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
         context.startService(intentToSyncImmediately);
+    }
+
+    private static class fillContentProvider extends AsyncTask<Void, Void, Void> {
+
+        @SuppressLint("StaticFieldLeak")
+        Context mContext;
+
+        public fillContentProvider(Context context) { mContext = context; }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
+            String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+            String selectionStatement = WeatherContract.WeatherEntry
+                    .getSqlSelectForTodayOnwards();
+
+            Cursor cursor = mContext.getContentResolver().query(
+                    forecastQueryUri
+                    ,projectionColumns
+                    ,selectionStatement
+                    ,null
+                    ,null
+            );
+
+            //  COMPLETED (5) Check to see if our weather ContentProvider is empty
+            if (cursor == null || cursor.getCount() == 0) {
+                //  COMPLETED (6) If it is empty or we have a null Cursor, sync the weather now!
+                startImmediateSync(mContext);
+            }
+
+            if (cursor != null) {
+                cursor.close();
+            }
+
+            return null;
+        }
+
     }
 }
